9a311b0169d33f66850c5f19cf993904f997db53
Ross Wu <RAWSE-DA-BAWSE@Rosss-MacBook-Pro.local>
2017-05-31 19:41:18 -1000

    not done, stuck on add

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,54 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+    var head = null;
+    var tail = null;
 
+
+   function getHead(){
+    return head;
+   }
+
+   function getTail(){
+    return tail;
+   }
+
+   function add(head, tail){
+
+      var node = {};
+      node.value = head;
+      node.next = getTail();
+    
+    if(node.next === null){
+      return node;
+     }else{
+        var newNode = {};
+        newNode.value = head;
+        newNode.next = tail;
+        return
+      }
+    }
+
+
+   function remove(){
+    return remove;
+   }
+
+   function get(){
+    return get;
+   }
+
+   function insert(){
+    return insert;
+   }
+
+     return { // this returns module functions (objects)
+        getHead: getHead,
+        getTail: getTail,
+        add: add,
+        remove: remove,
+        get: get,
+        insert:insert,
+
+    };
 }
